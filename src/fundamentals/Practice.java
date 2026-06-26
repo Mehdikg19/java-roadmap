c6cc63f61e6d49c49b020a2f773c322f42bf7b08
Mehdikg19 <karimigale.mehdi@gmail.com>
2026-06-26 23:15:42 +0330

    Add Practice.java template

--- src/fundamentals/Practice.java
+++ src/fundamentals/Practice.java
@@ -1,4 +1,21 @@
+/*
+ * ============================================================
+ * Java Roadmap
+ * Topic: Practice
+ * File: Practice.java
+ * Description: This file is used for temporary code,
+ * experiments, and testing Java concepts during learning.
+ * Author: Mehdi
+ * ============================================================
+ */
 package fundamentals;
 
 public class Practice {
+
+    public static void main(String[] args) {
+
+        // Write your temporary code here.
+
+    }
+
 }
